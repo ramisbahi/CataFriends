76df42251551273c97bd6601ae2361bece846ee9
Rami Sbahi <sbahirami@gmail.com>
2022-11-03 21:29:15 -0400

    started mock data

--- CataTracker/ViewController.swift
+++ CataTracker/ViewController.swift
@@ -8,13 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let mockData: String = "[\"name\""
 
     @IBOutlet weak var MemberImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        // TODO: update elements
     }
 
 
